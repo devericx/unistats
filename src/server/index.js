161cda68b79764030434302cbc20cfe724bdc73b
devericx <devericx@gmail.com>
2018-04-19 22:11:05 -0400

    Add Let's Encrypt certificate

--- src/server/index.js
+++ src/server/index.js
@@ -4,5 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/.well-known/acme-challenge/gdr1iwKFlqCYll_L5n3jLtiRLSr8G9nMvSEEq776XBs', (req, res) => {
+  res.send('gdr1iwKFlqCYll_L5n3jLtiRLSr8G9nMvSEEq776XBs.nT1Djtm4_gDmWMaX8znAJp81RBcm4R9fWiv-IzQWt30');
+});
+
 app.use(express.static(`${__dirname}./../../`));
 app.listen(PORT);
