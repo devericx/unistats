ad0d489638ae11ae86ad9d7de923e6359d3f3519
devericx <devericx@gmail.com>
2018-04-19 20:59:30 -0400

    Edit server/index.js for production mode

--- src/server/index.js
+++ src/server/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.static(`${__dirname}./../../`));
-app.listen(5000);
+app.listen(PORT);
